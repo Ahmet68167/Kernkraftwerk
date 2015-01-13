78f0adaa015af7c1790a465c2fb88d9791a90062
Ahmet <Ahmet@192.168.1.50>
2015-01-13 22:55:29 +0100

    Methode ausgleichen hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/Waermetauscher.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/Waermetauscher.java
@@ -1,5 +1,13 @@
 package tpe.ueb5.ib04;
 
 public class Waermetauscher {
-
+	
+	public Waermetauscher() {
+		
+	}
+	
+	public void ausgleichen(Wasserkreislauf kreislauf1, Wasserkreislauf kreislauf2) {
+		
+	}
+	
 }
