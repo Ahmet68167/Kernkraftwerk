1f89e47ce6d7a249fe1258ea4b5407e0d5f0c7c9
Ahmet <Ahmet@192.168.1.50>
2015-01-13 23:33:29 +0100

    ausgleichen-Methode und Attribute reaktorTemperatur, flussTemperatur, Wasserkreislauf hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/Waermetauscher.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/Waermetauscher.java
@@ -2,12 +2,43 @@ package tpe.ueb5.ib04;
 
 public class Waermetauscher {
 	
-	public Waermetauscher() {
-		
+	private int reaktorTemperatur;
+	private int flussTemperatur;
+	private Wasserkreislauf wasserkreislauf;
+	
+	public Waermetauscher(Wasserkreislauf wasserkreislauf) {
+		this.reaktorTemperatur = 10;
+		this.flussTemperatur = 10;
+		this.wasserkreislauf = wasserkreislauf;
 	}
 	
-	public void ausgleichen(Wasserkreislauf kreislauf1, Wasserkreislauf kreislauf2) {
-		
+	public int ausgleichen() {
+		return (this.wasserkreislauf.getKuehlkreislauf().element().getTemperatur() 
+				+ this.reaktorTemperatur) / 2;
+	}
+
+	public int getReaktorTemperatur() {
+		return this.reaktorTemperatur;
+	}
+
+	public void setReaktorTemperatur(int reaktorTemperatur) {
+		this.reaktorTemperatur = reaktorTemperatur;
+	}
+
+	public int getFlussTemperatur() {
+		return this.flussTemperatur;
+	}
+
+	public void setFlussTemperatur(int flussTemperatur) {
+		this.flussTemperatur = flussTemperatur;
+	}
+
+	public Wasserkreislauf getWasserkreislauf() {
+		return this.wasserkreislauf;
+	}
+
+	public void setWasserkreislauf(Wasserkreislauf wasserkreislauf) {
+		this.wasserkreislauf = wasserkreislauf;
 	}
 	
 }
