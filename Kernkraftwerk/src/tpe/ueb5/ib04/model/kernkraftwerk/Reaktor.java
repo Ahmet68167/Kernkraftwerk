51709044a1d6150782fd8b9c458c7da73ac497d3
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:44:05 +0100

    Javadoc hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Reaktor.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Reaktor.java
@@ -3,6 +3,10 @@ package tpe.ueb5.ib04.model.kernkraftwerk;
 import tpe.ueb5.ib04.control.Kernkraftwerk;
 import tpe.ueb5.ib04.view.Leitware;
 
+/**
+ * Legt einen Reaktor an, der Abwaerme erzeugt und auf eine Kernschmelze prueft
+ *
+ */
 public class Reaktor implements Runnable {
 
 	private final int KRITISCHE_TEMPERATUR = 2878;
@@ -11,10 +15,19 @@ public class Reaktor implements Runnable {
 	private boolean kernschmelze;
 
 	
+	/**
+	 * Legt einen Reaktor mit dem Erwaermungskoeffizienten von 42° an
+	 * 
+	 */
 	public Reaktor() {
 		this(42);
 	}
 	
+	/**
+	 * Legt einen Reaktor mit dem angegeben Erwaermungskoeffizienten an
+	 * 
+	 * @param erwaermungskoeffizient - Erwaermungskoeffizient für den Reaktor
+	 */
 	public Reaktor(int erwaermungskoeffizient) {
 		this.erwaermungskoeffizient = erwaermungskoeffizient;
 		this.abwaerme = 10;
@@ -50,26 +63,56 @@ public class Reaktor implements Runnable {
 		
 	}
 
+	/**
+	 * Gibt den Erwaermungskoeffizienten des Reaktors zurueck
+	 * 
+	 * @return den Erwaermungskoeffizienten
+	 */
 	public int getErwaermungskoeffizient() {
 		return this.erwaermungskoeffizient;
 	}
 
+	/**
+	 * Setzt den Erwaermungskoeffizienten für den Reaktor
+	 * 
+	 * @param erwaermungskoeffizient - Erwaermungskoeffizient für den Reaktor
+	 */
 	public void setErwaermungskoeffizient(int erwaermungskoeffizient) {
 		this.erwaermungskoeffizient = erwaermungskoeffizient;
 	}
 
+	/**
+	 * Gibt die erzeugte Abwaerme zurueck
+	 * 
+	 * @return die erzeugte Abwaerme
+	 */
 	public int getAbwaerme() {
 		return this.abwaerme;
 	}
 
+	/**
+	 * Setzt die Abwaerme die durch den Reaktor erzeugt wird
+	 * 
+	 * @param abwaerme - Abwaerme die durch den Reaktor erzeugt wird
+	 */
 	public void setAbwaerme(int abwaerme) {
 		this.abwaerme = abwaerme;
 	}
 
+	/**
+	 * Gibt zurueck ob eine Kernschmleze stattgefunden hat
+	 * 
+	 * @return ob eine Kernschmelze stattgefunden hat
+	 */
 	public boolean isKernschmelze() {
 		return this.kernschmelze;
 	}
 
+	/**
+	 * Setzt den Wert für die Kernschmelze
+	 * 
+	 * @param kernschmelze - Kernschmelze 
+	 */
 	public void setKernschmelze(boolean kernschmelze) {
 		this.kernschmelze = kernschmelze;
 	}
