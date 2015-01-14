591aa1cb9d66c06267932eac46b5572ff2de3ad1
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:45:41 +0100

    Javadoc hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Waermetauscher.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Waermetauscher.java
@@ -2,12 +2,20 @@ package tpe.ueb5.ib04.model.kernkraftwerk;
 
 
 
+/**
+ * Legt einen Waermetauscher an, der die Temperatur zwischen einem Wasserelement und einer Temperatur
+ * ausgleicht
+ *
+ */
 public class Waermetauscher {
 	
-	public Waermetauscher() {
-
-	}
-	
+	/**
+	 * Gleicht die Temperatur zwischen Wasserelement und der übergebenen Temperatur aus
+	 * 
+	 * @param element - Wasserelement, dessen Temperatur ausgeglichen werden soll
+	 * @param temperatur - Temperatur mit der ausgeglichen werden soll
+	 * @return
+	 */
 	public int ausgleichen(Wasserelement element, int temperatur) {
 		int ausgangsTemperatur = berechneTemperatur(element.getTemperatur(), temperatur);
 		element.setTemperatur(ausgangsTemperatur);
