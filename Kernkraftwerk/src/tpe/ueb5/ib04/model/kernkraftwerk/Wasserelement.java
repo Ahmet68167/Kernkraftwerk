ac3181f1bad0ad9c0336ece322e840a5c79c25c2
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:47:05 +0100

    Javadoc hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Wasserelement.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Wasserelement.java
@@ -1,17 +1,35 @@
 package tpe.ueb5.ib04.model.kernkraftwerk;
 
+/**
+ * Legt ein Wasserelement mit einer bestimmten Temperatur an
+ *
+ */
 public class Wasserelement {
 
 	private int temperatur;
 	
+	/**
+	 * Legt ein Wasserelement mit einer bestimmten Temperatur an
+	 * 
+	 */
 	public Wasserelement() {
 		this.temperatur = Wasserkreislauf.TEMPERATUR;
 	}
 
+	/**
+	 * Gibt die Temperatur des Wasserelements zurueck
+	 * 
+	 * @return die Temperatur des Wasserelements
+	 */
 	public int getTemperatur() {
 		return this.temperatur;
 	}
 
+	/**
+	 * Setzt die Temperatur des Wasserelements
+	 * 
+	 * @param temperatur - Temperatur des Wasserelements
+	 */
 	public void setTemperatur(int temperatur) {
 		this.temperatur = temperatur;
 	}
