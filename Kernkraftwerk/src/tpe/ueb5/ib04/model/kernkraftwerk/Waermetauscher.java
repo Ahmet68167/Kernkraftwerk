7ee3b1876f712c012a236e83c05c87e9d849a3b9
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:21:42 +0100

    packetstruktur geändert

---
+++ Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Waermetauscher.java
@@ -0,0 +1,21 @@
+package tpe.ueb5.ib04.model.kernkraftwerk;
+
+
+
+public class Waermetauscher {
+	
+	public Waermetauscher() {
+
+	}
+	
+	public int ausgleichen(Wasserelement element, int temperatur) {
+		int ausgangsTemperatur = berechneTemperatur(element.getTemperatur(), temperatur);
+		element.setTemperatur(ausgangsTemperatur);
+		return ausgangsTemperatur;
+	}
+	
+	private int berechneTemperatur(int temperatur1, int temperatur2) {
+		return (temperatur1 + temperatur2) / 2;
+	}
+	
+}
