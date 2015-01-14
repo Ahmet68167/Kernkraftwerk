b923f288756be634dc052bf67954f178ed78320e
Ahmet <Ahmet@192.168.1.50>
2015-01-14 18:33:56 +0100

    einige Methoden implementiert + run Methode

--- Kernkraftwerk/src/tpe/ueb5/ib04/Pumpe.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/Pumpe.java
@@ -2,30 +2,67 @@ package tpe.ueb5.ib04;
 
 public class Pumpe implements Runnable {
 
+	private Reaktor reaktor;
 	private Wasserkreislauf wasserkreislauf;
+	private Waermetauscher waermetauscher;
 	private int pumpenkoeffizient;
 	
-	public Pumpe(Wasserkreislauf wasserkreislauf, int pumpenkoeffizient) {
+	public Pumpe(Reaktor reaktor, Wasserkreislauf wasserkreislauf, int pumpenkoeffizient) {
+		this.reaktor = reaktor;
 		this.wasserkreislauf = wasserkreislauf;
+		this.waermetauscher = new Waermetauscher();
 		this.pumpenkoeffizient = pumpenkoeffizient;
 	}
 
+	private void pumpen() {
+		gleicheWaermeAus();
+		//this.wasserkreislauf.rotiere();
+	}
+	
+	private void gleicheWaermeAus() {
+		this.reaktor.setAbwaerme(this.waermetauscher.ausgleichen(
+				this.wasserkreislauf.getKuehlkreislauf().peek(), this.reaktor.getAbwaerme()));
+		this.wasserkreislauf.getKuehlkreislauf().addLast(new Wasserelement());
+	}
+	
+	private String status() {
+		return "Temperatur Reaktor: " + this.reaktor.getAbwaerme() 
+				+ ", Temperatur Rueckfluss in Rhein " 
+				+ this.wasserkreislauf.getKuehlkreislauf().pop().getTemperatur();
+	}
+	
 	@Override
 	public void run() {
 		
 		while(true) {
-			this.wasserkreislauf.rotiere();
-		
-			try {
-				this.wait(1000/this.pumpenkoeffizient);
-			} catch (InterruptedException e) {
 			
-				e.printStackTrace();
+			synchronized(Leitware.LOCK) {
+			
+				pumpen();
+				System.out.println(status());
+				this.wasserkreislauf.rotiere();
+				
+				try {
+					Leitware.LOCK.wait(1000/this.pumpenkoeffizient);
+				} catch (InterruptedException e) {
+					System.out.println("Die Pumpe wurde angehalten.");
+					e.printStackTrace();
+				}
+			
 			}
+		
 		}
 		
 	}
 	
+	public Reaktor getReaktor() {
+		return reaktor;
+	}
+
+	public void setReaktor(Reaktor reaktor) {
+		this.reaktor = reaktor;
+	}
+
 	public Wasserkreislauf getWasserkreislauf() {
 		return this.wasserkreislauf;
 	}
