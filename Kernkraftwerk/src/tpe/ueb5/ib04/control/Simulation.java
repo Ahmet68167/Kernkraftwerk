44e714d523f76af0ea65e0526131047734019267
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:20:16 +0100

    Simulation hinzugefuegt

---
+++ Kernkraftwerk/src/tpe/ueb5/ib04/control/Simulation.java
@@ -0,0 +1,26 @@
+package tpe.ueb5.ib04.control;
+
+public class Simulation {
+
+	public static void main(String[] args) {
+		
+		
+		Thread a = new Thread(new Kernkraftwerk(1));
+		a.start();
+		
+		/*
+		Thread b = new Thread(new Kernkraftwerk(1, 2000, 350));
+		b.start();
+		
+		
+		Thread c = new Thread(new Kernkraftwerk(2));
+		c.start();
+		
+		
+		Thread d = new Thread(new Kernkraftwerk(1, 10, 100));
+		d.start();
+		*/
+		
+	}
+
+}
