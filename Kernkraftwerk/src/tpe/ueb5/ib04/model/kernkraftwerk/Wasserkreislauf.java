910ffee9067339196370ff043045f534b2b9fa17
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:22:17 +0100

    Temperatur für wasserelement hinzugefuegt

---
+++ Kernkraftwerk/src/tpe/ueb5/ib04/model/kernkraftwerk/Wasserkreislauf.java
@@ -0,0 +1,34 @@
+package tpe.ueb5.ib04.model.kernkraftwerk;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Wasserkreislauf {
+
+	private final int ANZAHL_ELEMENTE = 12;
+	public static int TEMPERATUR = 10;
+	private Deque<Wasserelement> kuehlkreislauf = new LinkedList<>();
+	
+	public Wasserkreislauf() {
+		fuelleKuehlkreislauf();
+	}
+	
+	public void fuelleKuehlkreislauf() {
+		for(int i = 0; i < ANZAHL_ELEMENTE; i++) {
+			this.kuehlkreislauf.addLast(new Wasserelement());
+		}
+	}
+	
+	public void rotiere() {
+		this.kuehlkreislauf.addLast(this.kuehlkreislauf.pop());
+	}
+	
+	public Deque<Wasserelement> getKuehlkreislauf() {
+		return this.kuehlkreislauf;
+	}
+
+	public void setKuehlkreislauf(Deque<Wasserelement> kuehlkreislauf) {
+		this.kuehlkreislauf = kuehlkreislauf;
+	}
+	
+}
