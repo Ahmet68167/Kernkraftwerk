8f11e947031bd9afd077fcdff6857a2d5db5dd67
Ahmet <Ahmet@192.168.1.50>
2015-01-14 21:29:57 +0100

    Javadoc hinzugefuegt

--- Kernkraftwerk/src/tpe/ueb5/ib04/control/Kernkraftwerk.java
+++ Kernkraftwerk/src/tpe/ueb5/ib04/control/Kernkraftwerk.java
@@ -4,6 +4,11 @@ import tpe.ueb5.ib04.model.kernkraftwerk.Pumpe;
 import tpe.ueb5.ib04.model.kernkraftwerk.Reaktor;
 import tpe.ueb5.ib04.model.kernkraftwerk.Wasserkreislauf;
 
+/**
+ * Verwaltet die einzelnen Bestandteile eines Kernkraftwerkes und simuliert somit ein Kernkraftwerk.
+ * Implementiert das Interface Runnable damit man sie als Thread ausführen kann.
+ *
+ */
 public class Kernkraftwerk implements Runnable {
 	
 	private Reaktor reaktor;
@@ -11,14 +16,35 @@ public class Kernkraftwerk implements Runnable {
 	private Pumpe pumpe;
 	public static volatile boolean running = true;
 	
+	/**
+	 * Legt ein Kernkraftwerk mit der angegebenen Pumpleistung, einer Flusstemperatur von 10°
+	 * und mit dem Erwaermungskoeffizienten 42° an
+	 * 
+	 * @param pumpleistung - Die Leistung, mit der die Pumpe arbeiten soll
+	 */
 	public Kernkraftwerk(double pumpleistung) {
 		this(pumpleistung, 10, 42);
 	}
 	
+	/**
+	 * Legt ein Kernkraftwerk mit der angegebenen Pumpleistung, Flusstemperatur
+	 * und mit dem Erwaermungskoeffizienten 42° an
+	 * 
+	 * @param pumpleistung - Die Leistung, mit der die Pumpe arbeiten soll
+	 * @param flusstemperatur - Die Temperatur des Flusses
+	 */
 	public Kernkraftwerk(double pumpleistung, int flusstemperatur) {
 		this(pumpleistung, flusstemperatur, 42);
 	}
 	
+	/**
+	 * Legt ein Kernkraftwerk mit der angegebenen Pumpleistung, Flusstemperatur
+	 * und Erwaermungskoeffizienten an
+	 * 
+	 * @param pumpleistung - Die Leistung, mit der die Pumpe arbeiten soll
+	 * @param flusstemperatur - Die Temperatur des Flusses
+	 * @param erwaermungskoeffizient - Der Erwaermungskoeffizient für den Reaktor
+	 */
 	public Kernkraftwerk(double pumpleistung, int flusstemperatur, int erwaermungskoeffizient) {
 		Wasserkreislauf.TEMPERATUR = flusstemperatur;
 		this.reaktor = new Reaktor(erwaermungskoeffizient);
